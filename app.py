08c3e33f85ce8faa1b501e4040af12e36c869ebe
Vikrant Mehta <vikrantmehta123@gmail.com>
2024-08-21 15:35:53 +0530

    Added the selenium driver code

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
 import re
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # region configuration
 load_dotenv(r"..\secrets.env")
@@ -19,25 +24,42 @@ headers = {
 }
 
 cookies = {
-    "csrftoken":CSRFTOKEN,
+    "csrftoken" :CSRFTOKEN,
     "sessionid": SESSION_ID
 }
 
 # endregion
 
-def parse_company_data(href_tag):
+def expand_hidden_rows(driver, href_tag):
+    driver.get(f"https://www.screener.in/{href_tag}/")
+
+    buttons = driver.find_elements(By.CSS_SELECTOR, '#balance-sheet button.button-plain')
+
+    for button in buttons:
+        try:
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable(button))
+            button.click()
+        except Exception as e:
+            print(e)
+
+    # Wait for the entire content to load properly
+    driver.implicitly_wait(15)
+
+    # Get the page source after the content has been expanded
+    html_content = driver.page_source
+    return html_content
+
+def parse_company_data(html_content):
     """
     Parses the company data and returns an object that holds important parameters for free puffs calculation
     """
-    res = requests.get(f"https://www.screener.in/{href_tag}", cookies=cookies, headers=headers)
-    soup = BeautifulSoup(res.content, features='html.parser')
+    soup = BeautifulSoup(html_content, features='html.parser')
     rows = soup.find(id='balance-sheet').find('table').find_all("tr")[1:]
     
     data = { }
 
     for row in rows:
         cells = row.find_all('td')
-        print(cells[0])
         
         # Select the last cell in the row
         if cells:  # Check if there are any <td> elements in the row
@@ -58,14 +80,21 @@ def compute_free_puff_valuation(company_data):
 
     assets = 0
     if "Fixed Assets" in company_data: assets += max(0.25 * float(company_data['Fixed Assets']), 0)
-    if "Investments" in company_data: assets += max(company_data["Investments"], 0)
+    if "Investments" in company_data: assets += max(float(company_data["Investments"]), 0)
 
-def parse_screen(screen_url):
+    return assets - debt
+
+def parse_screen(screen_url, driver:webdriver.Chrome):
     """
     Parses a query screen 
     """
-    response = requests.get(f"{screen_url}", cookies=cookies, headers=headers)
 
+    df = [ ]
+    response = requests.get(f"{screen_url}", cookies=cookies, headers=headers)
+    if response.status_code == 404:
+        sys.exit("Please update the cookies or check the URL")
+    if response.status_code != 200:
+        sys.exit("Something went wrong. Retry!")
     soup = BeautifulSoup(response.content, features='html.parser')
     table = soup.find('table')
     # Iterate over each row in the table, skipping the header
@@ -73,12 +102,20 @@ def parse_screen(screen_url):
         # Extract the cells in the row
         a_tag = row.find('a')
         href_val = a_tag['href']
-        obj = parse_company_data(href_tag=href_val)
-    
+        html_content = expand_hidden_rows(driver=driver, href_tag=href_val)
+        data = parse_company_data(html_content=html_content)
+        valuation =  compute_free_puff_valuation(company_data=data)
+        data['valuation'] = valuation
+        df.append(data)
+        print(data)
+    return df
 
 def main():
     screen_url = input("Enter screen URL: ")
-    parse_screen(screen_url=screen_url)
+    driver = webdriver.Chrome()
+    driver.fullscreen_window()
+    parse_screen(screen_url=screen_url, driver=driver)
+    driver.quit()
 
 if __name__ == "__main__":
     main()
