2825724147f8b0cd4103b76d6932ae3d26ec6645
Vikrant Mehta <vikrantmehta123@gmail.com>
2024-08-24 02:57:17 +0530

    Added comments

--- test_scraper.py
+++ test_scraper.py
@@ -4,7 +4,10 @@ from selenium import webdriver
 # close to what you got with the code.
 
 def test_valuation_of_baroda_rayon():
+    """Baroda Rayons Ltd has a company id of: 500270"""
+
     driver = webdriver.Chrome()
+    # The below values are computed on 24th August 2024 manually using Screener
     manually_computed_debt = 309
     manually_computed_assets = 415
     manual_valuation = manually_computed_assets - manually_computed_debt
@@ -13,23 +16,26 @@ def test_valuation_of_baroda_rayon():
     auto_computed_assets, auto_computed_debt =  compute_free_puff_valuation(company_data=data)
     auto_computed_valuation = auto_computed_assets - auto_computed_debt
 
-    print(data, manual_valuation, auto_computed_valuation)
-    # Check whether auto computation is within 10% of what you computed naturally
-    assert abs(auto_computed_valuation - manual_valuation) <= manual_valuation * 0.1
-
     driver.quit()
 
+    # Check whether auto computation is within 10% of what you computed manually
+    assert abs(auto_computed_valuation - manual_valuation) <= manual_valuation * 0.1
+
 def test_valuation_of_family_care_hospitals():
+    """Family Care Hospitals Ltd has a company id of: 516110"""
     driver = webdriver.Chrome()
+
+    # The below values are computed on 24th August 2024 manually using Screener
     manually_computed_debt = 12
     manually_computed_assets = 52
     manual_valuation = manually_computed_assets - manually_computed_debt
+    
     html_content = expand_hidden_rows(driver=driver, href_tag="company/516110/")
     data = parse_company_data(html_content=html_content, href_tag="company/516110/")
     auto_computed_assets, auto_computed_debt =  compute_free_puff_valuation(company_data=data)
     auto_computed_valuation = auto_computed_assets - auto_computed_debt
-
-    # Check whether auto computation is within 10% of what you computed naturally
-    assert abs(auto_computed_valuation - manual_valuation) <= manual_valuation * 0.1
-
+    
     driver.quit()
+
+    # Check whether auto computation is within 10% of what you computed manually
+    assert abs(auto_computed_valuation - manual_valuation) <= manual_valuation * 0.1
